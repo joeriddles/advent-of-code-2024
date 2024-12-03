3a71ce88f3cafbb5f8012b0032475dfb3b5d641e
joeriddles <joeriddles10@gmail.com>
2024-12-03 07:42:28 -0700

    Start day 3 part 2

--- 2024/day3/main.go
+++ 2024/day3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -30,7 +31,11 @@ func NewDay3() day.Day {
 	}
 }
 
-var MUL_PATTERN = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+var (
+	MUL_PATTERN  = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	DO_PATTERN   = regexp.MustCompile(`do\(\)`)
+	DONT_PATTERN = regexp.MustCompile(`don't\(\)`)
+)
 
 func (d *Day3) Part1(input string) int {
 	result := 0
@@ -43,7 +48,10 @@ func (d *Day3) Part1(input string) int {
 
 func (d *Day3) Part2(input string) int {
 	result := 0
-
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		result += d.findMulWithDos(line)
+	}
 	return result
 }
 
@@ -58,4 +66,93 @@ func (d *Day3) findMul(input string) int {
 		result += l * r
 	}
 	return result
+}
+
+/* TODO(joeriddles): Use a state machine */
+func (d *Day3) findMulWithDos(input string) int {
+	result := 0
+
+	dos := d.getIndexes(DO_PATTERN.FindAllStringSubmatchIndex(input, -1))
+	donts := d.getIndexes(DONT_PATTERN.FindAllStringSubmatchIndex(input, -1))
+	muls := MUL_PATTERN.FindAllStringSubmatchIndex(input, -1)
+
+	d.printMatches(input, dos, donts, muls)
+
+	for _, match := range muls {
+		enabled := d.isEnabled(match[0], &dos, &donts)
+		if enabled {
+			lstr := input[match[2]:match[3]]
+			rstr := input[match[4]:match[5]]
+			l, _ := strconv.Atoi(lstr)
+			r, _ := strconv.Atoi(rstr)
+			result += l * r
+		}
+	}
+
+	return result
+}
+
+func (d *Day3) isEnabled(i int, dos *[]int, donts *[]int) bool {
+	lastDo := -1
+	lastDont := -1
+
+	for _, j := range *dos {
+		if j >= i {
+			break
+		}
+		lastDo = j
+	}
+
+	for _, j := range *donts {
+		if j >= i {
+			break
+		}
+		lastDont = j
+	}
+
+	return lastDo == -1 && lastDont == -1 || lastDo > lastDont
+}
+
+func (d *Day3) getIndexes(matches [][]int) []int {
+	indexes := []int{}
+	for _, match := range matches {
+		indexes = append(indexes, match[0])
+	}
+	return indexes
+}
+
+const (
+	Reset string = "\033[0m"
+	Red   string = "\033[31m"
+	Green string = "\033[32m"
+)
+
+func (d *Day3) printMatches(input string, dos []int, donts []int, muls [][]int) {
+	mulIndexes := d.getIndexes(muls)
+
+	enabled := true
+	for i := range input {
+		if slices.Contains(dos, i) {
+			enabled = true
+			fmt.Printf(Reset+"%4v: %v\n", i, input[i:i+4])
+		}
+		if slices.Contains(donts, i) {
+			enabled = false
+			fmt.Printf(Reset+"%4v: %v\n", i, input[i:i+7])
+		}
+		if slices.Contains(mulIndexes, i) {
+			for _, mul := range muls {
+				color := Green
+				if !enabled {
+					color = Red
+				}
+
+				if mul[0] == i {
+					fmt.Printf(color+"%4v: %v\n", i, input[mul[0]:mul[1]])
+					break
+				}
+			}
+		}
+	}
+	fmt.Println("-------")
 }
--- 2024/day3/main_test.go
+++ 2024/day3/main_test.go
@@ -2,9 +2,8 @@ package main
 
 import "testing"
 
-const src = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
-
 func TestPart1(t *testing.T) {
+	src := `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`
 	expected := 161
 	actual := (&Day3{}).Part1(src)
 	if actual != expected {
@@ -13,7 +12,17 @@ func TestPart1(t *testing.T) {
 }
 
 func TestPart2(t *testing.T) {
+	src := `xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`
 	expected := 48
+	actual := (&Day3{}).Part2(src)
+	if actual != expected {
+		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestPart2_Custom(t *testing.T) {
+	src := `mul(1,1)don't()mul(1,2)do()mul(1,3)`
+	expected := 4
 	actual := (&Day3{}).Part2(src)
 	if actual != expected {
 		t.Fatalf("expected %v, got %v", expected, actual)
