fa9da4c1cb077b4207ad3478cfb7a281232fb158
joeriddles <joeriddles10@gmail.com>
2024-12-02 19:54:59 -0700

    Add top-level main.go

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
 
+// Solve all days
+func main() {
+	matches, _ := filepath.Glob("2024/day*/main.go")
+	for _, fp := range matches {
+		fmt.Printf("-- %v\n", fp)
+		filename := filepath.Base(fp)
+		cmd := exec.Command("go", "run", filename, "input.txt")
+		cmd.Dir = filepath.Dir(fp)
+		stdout, stderr := cmd.CombinedOutput()
+		if len(stdout) > 0 {
+			fmt.Printf("%v\n", string(stdout))
+		}
+		if stderr != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", stderr.Error())
+		}
+	}
 }
